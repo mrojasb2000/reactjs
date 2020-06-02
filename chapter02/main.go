2cb382cd06659e05f0b8122aa24b48dca5b28bb3
Mauricio Rojas <mrojasb2000@gmail.com>
2020-06-02 19:45:09 -0400

    Add basic usr struct.

---
+++ chapter02/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	Status    bool
+}
+
+func main() {
+	var usr = new(user)
+	usr.ID = 1
+	usr.Name = "Mavro"
+	usr.CreatedAt = time.Now()
+	usr.Status = true
+
+	fmt.Printf("\nID: %d", usr.ID)
+	fmt.Printf("\nName: %s", usr.Name)
+	fmt.Printf("\nCreated: %s", usr.CreatedAt)
+	fmt.Printf("\nStatus: %t\n", usr.Status)
+}
